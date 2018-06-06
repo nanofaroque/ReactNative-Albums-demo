10ba11940a1280df732dc834a4caa07b4b5076c5
Md Faroque <Omarf.android@gmail.com>
2018-06-06 02:05:51 -0500

    rendering album only

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,6 +3,10 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    // This is just initial state. we need to update this by calling setState() method
+    state = {
+        albums : []
+    }
     componentWillMount() {
         console.log('component will mount in albumlist ');
         this.fetchData();
@@ -10,13 +14,18 @@ class AlbumList extends Component {
     fetchData() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
         .then(response => {
-            console.log(response);
+            this.setState({ albums: response.data });
         });
       }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
+    }
     render() {
+        console.log(this.state);
     return (
         <View>
-            <Text>Album List !!</Text>
+            {this.renderAlbums()}
         </View>
     );
 }
