cc3882f58485ab8001f761b19c489f7048af550c
Md Faroque <Omarf.android@gmail.com>
2018-06-06 19:53:18 -0500

    updated list with thumbnail image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import Card from './ItemCard';
+import CardSection from './CardSection';
 
 // Since this is just a presentation component, we wil go with functional component
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ record }) => {
+    const { title, artist, thumbnail_image } = record;
     return (
-        <View>
-            <Text>{props.record.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <View>
+                    <Image 
+                        style={styles.thumbnail_image_style}
+                        source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={styles.headerContentStyle}>
+                <Text>{title}</Text>
+                <Text>{artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
     );
-
 };
-
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    thumbnail_image_style: {
+        height: 55,
+        width: 55
+    }
+};
 export default AlbumDetail;
